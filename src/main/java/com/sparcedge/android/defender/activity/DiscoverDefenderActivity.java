c658b11f0a78ae7f85d12ebd2ef6b28c9124692f
Dayel Ostraco <dayel.ostraco@gmail.com>
2012-12-18 16:49:34 -0500

    added connected UI screen. created BluetoothDeviceAdapter

--- src/main/java/com/sparcedge/android/defender/activity/DiscoverDefenderActivity.java
+++ src/main/java/com/sparcedge/android/defender/activity/DiscoverDefenderActivity.java
@@ -3,46 +3,48 @@ package com.sparcedge.android.defender.activity;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 import com.sparcedge.android.defender.R;
-import com.sparcedge.android.defender.adapter.DefenderAdapter;
-import com.sparcedge.android.defender.model.Defender;
+import com.sparcedge.android.defender.adapter.BluetoothDeviceAdapter;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+/**
+ * TODO: Will need to multi-thread this process sine Bluetooth connections are slow and will freeze the UI.
+ */
 public class DiscoverDefenderActivity extends Activity {
 
     private static final int REQUEST_ENABLE_BT = 1000;
-
+    private static final UUID APP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static String TAG = "Core Defender for Android";
-    private BluetoothAdapter bluetoothAdapter;
-    private DefenderAdapter deviceListAdapter;
-    private List<Defender> bluetoothDeviceList = new ArrayList<Defender>();
-    private Defender selectedDefender;
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
+    //Bluetooth Members
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothDeviceAdapter deviceListAdapter;
+    private List<BluetoothDevice> bluetoothDeviceList = new ArrayList<BluetoothDevice>();
+    private BluetoothDevice selectedBluetoothDevice;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
-        setContentView(R.layout.discover);
-
+        Log.i(TAG, "onCreate");
         enableBluetooth();
-        setUpButtons();
-        setUpListView();
+        inflateDiscoverInterface();
     }
 
     @Override
@@ -51,46 +53,117 @@ public class DiscoverDefenderActivity extends Activity {
     }
 
     @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
     }
 
     @Override
     protected void onDestroy() {
+        super.onDestroy();
+
         unregisterReceiver(bluetoothBroadcastReceiver);
-        bluetoothAdapter.cancelDiscovery();
+
+        if(bluetoothAdapter.isDiscovering()){
+            bluetoothAdapter.cancelDiscovery();
+        }
     }
 
-    private void setUpButtons() {
+    private void inflateDiscoverInterface() {
+        setContentView(R.layout.discover);
+        setUpDiscoverButtons();
+        setUpDiscoverListView();
+    }
+
+    private void inflateConnectionInterface() {
+        setContentView(R.layout.connect);
+        setUpConnectionButtons();
+    }
+
+    private void setUpDiscoverButtons() {
 
         final Button discoverDevicesButton = (Button) findViewById(R.id.discoverDevices);
         discoverDevicesButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 bluetoothDeviceList.clear();
+                deviceListAdapter.notifyDataSetChanged();
                 bluetoothAdapter.startDiscovery();
 
                 // Register the BroadcastReceiver
-                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                IntentFilter filter = new IntentFilter(android.bluetooth.BluetoothDevice.ACTION_FOUND);
                 registerReceiver(bluetoothBroadcastReceiver, filter);
             }
         });
     }
 
-    private void setUpListView() {
+    private void setUpDiscoverListView() {
         final ListView deviceListView = (ListView) findViewById(R.id.devicesList);
-        deviceListAdapter = new DefenderAdapter(this, R.layout.defenderlistview, bluetoothDeviceList);
+        deviceListAdapter = new BluetoothDeviceAdapter(this, R.layout.devicelistview, bluetoothDeviceList);
 
         deviceListView.setAdapter(deviceListAdapter);
 
         deviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
-                selectedDefender = (Defender) (deviceListView.getItemAtPosition(myItemInt));
-                setContentView(R.layout.connect);
+                selectedBluetoothDevice = (BluetoothDevice) (deviceListView.getItemAtPosition(myItemInt));
+                inflateConnectionInterface();
             }
         });
     }
 
-    private BluetoothAdapter getBluetoothAdapter(){
+    private void setUpConnectionButtons() {
+        final Button lockUnlockButton = (Button) findViewById(R.id.lockUnlock);
+        lockUnlockButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                try{
+                    //Turn off discovery if it is still enabled
+                    if(bluetoothAdapter.isDiscovering()) {
+                        bluetoothAdapter.cancelDiscovery();
+                    }
+
+                    //Create socket
+                    BluetoothSocket mSocket = selectedBluetoothDevice.createRfcommSocketToServiceRecord(APP_UUID);
+                    mSocket.connect();
+
+                    String hello = "Hello World";
+                    OutputStream bluetoothOutputStream = mSocket.getOutputStream();
+                    bluetoothOutputStream.write(hello.getBytes());
+                    bluetoothOutputStream.close();
+
+                    mSocket.close();
+
+                } catch (IOException e){
+                    Log.e(TAG, "Could not connect to device");
+                }
+            }
+        });
+
+        final Button billOfLadingButton = (Button) findViewById(R.id.billOfLading);
+        billOfLadingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        final Button downloadActivityButton = (Button) findViewById(R.id.downloadActivity);
+        downloadActivityButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        final Button discoverOtherDevicesButton = (Button) findViewById(R.id.discoverOtherDevices);
+        discoverOtherDevicesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                inflateDiscoverInterface();
+            }
+        });
+    }
+
+    /**
+     * Returns the BluetoothAdapter for the Android device.
+     * @return
+     */
+    private BluetoothAdapter getBluetoothAdapter() {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
             Log.i(TAG, "No Bluetooth Adapter for Device");
@@ -99,11 +172,27 @@ public class DiscoverDefenderActivity extends Activity {
         return bluetoothAdapter;
     }
 
+    /**
+     * Enables Bluetooth on the Android device and fires up the Intent for the user to enable Bluetooth if it is not.
+     */
     private void enableBluetooth() {
         bluetoothAdapter = getBluetoothAdapter();
-        if (bluetoothAdapter!=null && !bluetoothAdapter.isEnabled()) {
+        if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    /**
+     * Enables Discoverable mode ad fires up the Intent that allows the user to enable discoverability.
+     */
+    private void ensureDiscoverable() {
+        Log.d(TAG, "ensure discoverable");
+        if (bluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
         }
     }
 
@@ -117,13 +206,18 @@ public class DiscoverDefenderActivity extends Activity {
             // When discovery finds a device
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
 
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Defender defender = new Defender(device.getName(), device.getAddress(), device);
+                // Get the BluetoothDeviceAdapter object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(android.bluetooth.BluetoothDevice.EXTRA_DEVICE);
 
-                if(!bluetoothDeviceList.contains(defender)){
-                    bluetoothDeviceList.add(defender);
+                if (!bluetoothDeviceList.contains(device)) {
+                    bluetoothDeviceList.add(device);
                     deviceListAdapter.notifyDataSetChanged();
+                }
+                // When discovery is finished, change the Activity title
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                setProgressBarIndeterminateVisibility(false);
+                if (deviceListAdapter.getCount() == 0) {
+                    //TODO - Update a label somewhere to alert the user that no devices we found.
                 }
             }
         }
--- src/main/java/com/sparcedge/android/defender/adapter/BluetoothDeviceAdapter.java
+++ src/main/java/com/sparcedge/android/defender/adapter/BluetoothDeviceAdapter.java
@@ -1,5 +1,6 @@
 package com.sparcedge.android.defender.adapter;
 
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.sparcedge.android.defender.R;
-import com.sparcedge.android.defender.model.Defender;
 
 import java.util.List;
 
@@ -17,42 +17,42 @@ import java.util.List;
  * Date: 12/17/12
  * Time: 4:35 PM
  */
-public class DefenderAdapter extends ArrayAdapter {
+public class BluetoothDeviceAdapter extends ArrayAdapter {
 
     int resource;
     String response;
     Context context;
 
-    public DefenderAdapter(Context context, int resource, List<Defender> defenders) {
-        super(context, resource, defenders);
+    public BluetoothDeviceAdapter(Context context, int resource, List<BluetoothDevice> bluetoothDevices) {
+        super(context, resource, bluetoothDevices);
         this.resource = resource;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        RelativeLayout defenderView;
+        RelativeLayout deviceView;
         //Get the current alert object
-        Defender defender = (Defender) getItem(position);
+        BluetoothDevice device = (BluetoothDevice) getItem(position);
 
         //Inflate the view
         if (convertView == null) {
-            defenderView = new RelativeLayout(getContext());
+            deviceView = new RelativeLayout(getContext());
             String inflater = Context.LAYOUT_INFLATER_SERVICE;
             LayoutInflater vi;
             vi = (LayoutInflater) getContext().getSystemService(inflater);
-            vi.inflate(resource, defenderView, true);
+            vi.inflate(resource, deviceView, true);
         } else {
-            defenderView = (RelativeLayout) convertView;
+            deviceView = (RelativeLayout) convertView;
         }
 
         //Get the text boxes from the select_child_list_item.xml file
-        TextView deviceName = (TextView) defenderView.findViewById(R.id.deviceName);
-        TextView deviceMac = (TextView) defenderView.findViewById(R.id.deviceMac);
+        TextView deviceName = (TextView) deviceView.findViewById(R.id.deviceName);
+        TextView deviceMac = (TextView) deviceView.findViewById(R.id.deviceMac);
 
         //Assign the appropriate data from our defender object above
-        deviceName.setText(defender.getName());
-        deviceMac.setText(defender.getMacAddress());
+        deviceName.setText(device.getName());
+        deviceMac.setText(device.getAddress());
 
-        return defenderView;
+        return deviceView;
     }
 }
